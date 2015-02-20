bd5fa50377e500753b320a49e302385bb27040ec
Eric Fournier <me@ericdfournier.com>
2015-02-20 12:57:50 -0800

    Updated type definition source file to include exported type initialization functions.

--- types.go
+++ types.go
@@ -12,11 +12,12 @@ type Domain struct {
 	vals map[int]bool
 }
 
-func NewDomain(identifier, domainSize int, values map[int]bool) *Domain {
+// new domain initialization function
+func NewDomain(identifier, domainSize int, domainValues map[int]bool) *Domain {
 	return &Domain{
 		id:   identifier,
 		size: domainSize,
-		vals: values,
+		vals: domainValues,
 	}
 }
 
@@ -27,6 +28,15 @@ type Objective struct {
 	id      int
 	size    int
 	fitness map[int]float64
+}
+
+// new objective initialization function
+func NewObjective(identifier, objectiveSize int, fitnessValues map[int]float64) *Objective {
+	return &Objective{
+		id:      identifier,
+		size:    objectiveSize,
+		fitness: fitnessValues,
+	}
 }
 
 // individuals are comprised of row column indices to some
