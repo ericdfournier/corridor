e7c7df418bf18d65d064df7f732cbc36f4643214
Eric Fournier <me@ericdfournier.com>
2015-02-20 15:51:06 -0800

    Created new dedicated initialization source file for initializing custom types. Also, added toy initialization source file for generating valid toy test cases for use in development.

---
+++ initializations.go
@@ -0,0 +1,79 @@
+// Copyright ©2015 The corridor Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package corridor
+
+// new problem parameters function
+func NewParameters() {
+	
+	return &Parameters{
+		srcInd:
+		dstInd:
+		srcSub:
+		dstSub:
+		popSize:
+	}
+}
+
+// new domain initialization function
+func NewDomain(identifier, domainSize, domainStride int, domainValues []bool) 
+	*Domain {
+
+	//return output
+	return &Domain{
+		id:     identifier,
+		size:   domainSize,
+		stride: domainStride,
+		vals:   domainValues,
+	}
+}
+
+// new test domain initialization function
+func NewToyDomain(identifier, domainSize, domainStride int) *Domain {
+
+	// initialize value slice
+	domainValues := make([]bool, domainSize)
+
+	// loop through index values togo define domain
+	for i := 0; i < domainSize; i++ {
+		if i >= 0 && i < domainStride {
+			domainValues[i] = false
+		} else if i > (domainSize - domainStride) {
+			domainValues[i] = false
+		} else {
+			domainValues[i] = true
+		}
+	}
+	for i := 0; i < domainSize; i = (i + domainStride) {
+		domainValues[i] = false
+	}
+	for i := 1; i < domainSize; i = (i + domainStride) {
+		domainValues[i] = false
+	}
+
+	// return output
+	return &Domain{
+		id:     identifier,
+		size:   domainSize,
+		stride: domainStride,
+		vals:   domainValues,
+	}
+}
+
+// new objective initialization function
+func NewObjective(identifier int, fitnessValues []float64) *Objective {
+
+	// return output
+	return &Objective{
+		id:      identifier,
+		fitness: fitnessValues,
+	}
+}
+
+// new individual initialization function
+func NewIndividual(problemParameters Parameters, problemDomain Domain) 
+	*Individual {
+
+	//
+}
