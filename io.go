44649822ce0abd95ac737d851da6383448a451a1
Eric Fournier <me@ericdfournier.com>
2015-04-02 15:08:37 -0700

    Currently able to import and convert tiff and jpeg formatted domain reference images. Need to figure out a way to import geotiff images for the objectives such that the values are not converted into RGBA space. More work needed.

--- io.go
+++ io.go
@@ -41,7 +41,6 @@ func ImageToDomain(identifier int, inputImage image.Image) (outputDomain *Domain
 				domMat.Set(i, j, 0.0)
 			} else {
 				r, g, b, _ := inputImage.At(i-1, j-1).RGBA()
-
 				if r > 0 || g > 0 || b > 0 {
 					domMat.Set(i, j, 1.0)
 				} else {
@@ -58,8 +57,8 @@ func ImageToDomain(identifier int, inputImage image.Image) (outputDomain *Domain
 	return output
 }
 
-// function to convert an input RGBA formatted jpeg into
-// an output domain structure
+// function to import an input RGBA formatted jpeg and
+// convert it into an output domain structure
 func JpegToDomain(identifier int, inputFilepath string) (outputDomain *Domain) {
 
 	// read local jpeg image file
@@ -87,8 +86,8 @@ func JpegToDomain(identifier int, inputFilepath string) (outputDomain *Domain) {
 	return output
 }
 
-// function to convert an input RGBA formatted geotiff into
-// an output domain structure
+// function to import an input RGBA formatted geotiff and
+// convert it into an output domain structure
 func TiffToDomain(identifier int, inputFilepath string) (outputDomain *Domain) {
 
 	// read byte data
@@ -111,6 +110,105 @@ func TiffToDomain(identifier int, inputFilepath string) (outputDomain *Domain) {
 
 	// convert image to domain
 	output := ImageToDomain(identifier, img)
+
+	// return output
+	return output
+}
+
+// function to convert an input image structure
+// into and output objective structure
+func ImageToObjective(identifier int, inputImage image.Image) (outputObjective *Objective) {
+
+	// get input image boundaries
+	rows := inputImage.Bounds().Max.X - 1
+	cols := inputImage.Bounds().Max.Y - 1
+
+	// initialize domain matrix
+	objMat := mat64.NewDense(rows+2, cols+2, nil)
+
+	// write values from image object to domain matrix
+	for i := 0; i < rows+2; i++ {
+		for j := 0; j < cols+2; j++ {
+
+			// create a 1 pixel boundary buffer of zeros
+			if i == 0 {
+				objMat.Set(i, j, 0.0)
+			} else if i == rows+1 {
+				objMat.Set(i, j, 0.0)
+			} else if j == 0 {
+				objMat.Set(i, j, 0.0)
+			} else if j == cols+1 {
+				objMat.Set(i, j, 0.0)
+			} else {
+
+				//// NEED TO IMPOSE THAT THE INPUT JPEG IS FORMATTED AS A GRAYSCALE
+				//r, g, b, a := inputImage.At(i-1, j-1).RGBA()
+				//objMat.Set(i, j, r)
+
+			}
+		}
+	}
+
+	// initialize new domain
+	output := NewObjective(identifier, objMat)
+
+	// return output
+	return output
+}
+
+// function to import an input RGBA formatted jpeg and
+// convert it into an output objective structure
+func JpegToObjective(identifier int, inputFilepath string) (outputObjective *Objective) {
+
+	// read local jpeg image file
+	data, err := ioutil.ReadFile(inputFilepath)
+
+	// parse file not found error
+	if err != nil {
+		fmt.Println("File not found!")
+		os.Exit(1)
+	}
+
+	// decode input jpeg image
+	img, err := jpeg.Decode(bytes.NewReader(data))
+
+	// parse error
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// convert image to domain
+	output := ImageToObjective(identifier, img)
+
+	// return output
+	return output
+}
+
+// function to import an input RGBA formatted tiff and
+// convert it into an output objective structure
+func TiffToObjective(identifier int, inputFilepath string) (outputObjective *Objective) {
+
+	// read local jpeg image file
+	data, err := ioutil.ReadFile(inputFilepath)
+
+	// parse file not found error
+	if err != nil {
+		fmt.Println("File not found!")
+		os.Exit(1)
+	}
+
+	// decode input jpeg image
+	img, err := tiff.Decode(bytes.NewReader(data))
+
+	// parse error
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// convert image to domain
+	output := ImageToObjective(identifier, img)
 
 	// return output
 	return output
