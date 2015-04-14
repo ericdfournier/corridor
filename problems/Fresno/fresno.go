015a0465fcbc3594e06b08a264871c596b1fe765
Eric Fournier <me@ericdfournier.com>
2015-04-14 15:14:51 -0700

    Started a routine for testing the real world problem io procedure with Fresno...

---
+++ problems/Fresno/fresno.go
@@ -0,0 +1,64 @@
+// Copyright ©2015 The corridor Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ericdfournier/corridor"
+)
+
+func main() {
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// start clock
+	start := time.Now()
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// import domain
+	domainID := 1
+	searchDomain := corridor.CsvToDomain(domainID, "searchDomain.csv")
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// view domain
+	corridor.ViewDomain(searchDomain)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// NEED AN IO ROUTINE FOR IMPORTING MULTIPLE OBJECTIVES
+
+	// initialize objectives
+	objectiveID := 1
+	toyObjectives := corridor.NewToyObjectives(objectiveID, toyDomain.Rows, toyDomain.Cols, objectiveCount)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// initialize parameters
+	toyParameters := corridor.NewToyParameters(toyDomain)
+	toyParameters.SrcSubs[0] = 8
+	toyParameters.SrcSubs[1] = 14
+	toyParameters.DstSubs[0] = toyDomain.Rows - 8
+	toyParameters.DstSubs[1] = toyDomain.Rows - 14
+
+	//////////////////////////////////////////////////////////////////////////////////
+
+	// evolve populations
+	toyEvolution := corridor.NewEvolution(toyParameters, toyDomain, toyObjectives)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// view output population
+	corridor.ViewPopulation(toyDomain, toyParameters, <-toyEvolution.Populations)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// stop clock and print runtime
+	fmt.Printf("Elapsed Time: %s\n", time.Since(start))
+
+	///////////////////////////////////////////////////////////////////////////////////
+}
