3e6da8f9ad14aaad1d388a5d8654d7740b3dbe03
Eric Fournier <me@ericdfournier.com>
2015-04-22 09:39:29 -0700

    Lowered population size to 10000.

--- problems/Fresno/fresno.go
+++ problems/Fresno/fresno.go
@@ -48,8 +48,8 @@ func main() {
 	///////////////////////////////////////////////////////////////////////////////////
 
 	// initialize parameters
-	populationSize := 100000
-	evolutionSize := 10000
+	populationSize := 10000
+	evolutionSize := 1000
 	randomness := 1.0
 
 	// generate parameter structure
