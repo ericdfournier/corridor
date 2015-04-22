6b996e5e170b7432f5c291d6f97293b084d620bc
Eric Fournier <me@ericdfournier.com>
2015-04-22 08:06:46 -0700

    Added oxnard problem source routine as well as minor modifications to the other problem source routines in preparation for containerization.

--- problems/SanDiego/sanDiego.go
+++ problems/SanDiego/sanDiego.go
@@ -47,11 +47,12 @@ func main() {
 
 	///////////////////////////////////////////////////////////////////////////////////
 
-	//// initialize parameters
+	// initialize parameters
 	populationSize := 10000
 	evolutionSize := 1000
 	randomness := 1.0
 
+	// generate parameter structure
 	searchParameters := corridor.NewParameters(
 		source,
 		destination,
@@ -69,12 +70,15 @@ func main() {
 
 	///////////////////////////////////////////////////////////////////////////////////
 
+	// initialize elite count
 	eliteCount := 100
+
+	// extract elite set
 	eliteSet := corridor.NewEliteSet(eliteCount, <-searchEvolution.Populations)
 
 	///////////////////////////////////////////////////////////////////////////////////
 
-	// write chromosome to file
+	// write elite set to file
 	corridor.EliteSetToCsv(eliteSet, "sanDiego_eliteSet.csv")
 
 	///////////////////////////////////////////////////////////////////////////////////
