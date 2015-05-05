ac0cc33c5491375c0021222f1309f99d97030d25
Eric Fournier <me@ericdfournier.com>
2015-05-05 14:26:36 -0700

    Updated oxnard problem source file setting for rerun of the 100000 population size case.

--- problems/Oxnard/oxnard.go
+++ problems/Oxnard/oxnard.go
@@ -47,7 +47,7 @@ func main() {
 	///////////////////////////////////////////////////////////////////////////////////
 
 	// initialize parameters
-	populationSize := 1000
+	populationSize := 100000
 	evolutionSize := 1000
 	randomness := 1.0
 
