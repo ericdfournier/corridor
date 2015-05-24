c85ccdc6a5b21fe78cca97cddbda63f6acc4bd59
Eric Fournier <me@ericdfournier.com>
2015-05-24 16:38:08 -0700

    Added random source routine test suite. Need to develop stochastic tests to evaluate the random utilities in probabilistic terms.

---
+++ random_test.go
@@ -0,0 +1,15 @@
+// Copyright ©2015 The corridor Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package corridor
+
+import "testing"
+
+// test multivariaterandomnormal
+func TestMultiVariateRandomNormal(t *testing.T) {
+
+	// initialize test case
+	t.Log("MultiVariateRandomNormal Test: Expected Value")
+
+}
