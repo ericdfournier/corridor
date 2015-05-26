3aaf1bc492e4329f653e8eb0a0eee0d934aa73a3
Eric Fournier <me@ericdfournier.com>
2015-05-26 16:47:08 -0700

    Completed test suite routine for the multivariaterandomnormal function.

--- random_test.go
+++ random_test.go
@@ -4,12 +4,52 @@
 
 package corridor
 
-import "testing"
+import (
+	"math"
+	"testing"
+
+	"github.com/gonum/matrix/mat64"
+)
 
 // test multivariaterandomnormal
 func TestMultiVariateRandomNormal(t *testing.T) {
 
 	// initialize test case
-	t.Log("MultiVariateRandomNormal Test: Expected Value")
+	t.Log("MultiVariateRandomNormal Test: Expected Value = [1 +- 0.1 1 +- 0.1]")
 
+	// initialize expected mean
+	var muVec = []float64{1, 1}
+	mu := mat64.NewDense(2, 1, muVec)
+	var sigmaVec = []float64{1, 0, 0, 1}
+	sigma := mat64.NewSymDense(2, sigmaVec)
+
+	// initialize test case variables
+	testMat := mat64.NewDense(10000, 2, nil)
+	testCase := make([]float64, 2)
+	var testBool bool
+	var aSum float64 = 0.0
+	var bSum float64 = 0.0
+
+	// perform test case
+	for i := 0; i < 10000; i++ {
+		curVal := MultiVariateNormalRandom(mu, sigma)
+		testMat.Set(i, 0, curVal.At(0, 0))
+		testMat.Set(i, 1, curVal.At(1, 0))
+	}
+
+	// compute test result
+	for i := 0; i < 10000; i++ {
+		aSum += testMat.At(i, 0)
+		bSum += testMat.At(i, 1)
+	}
+	testCase[0] = aSum / 10000.0
+	testCase[1] = bSum / 10000.0
+	testBool = (math.Abs(1-testCase[0]) < 0.1) && (math.Abs(1-testCase[1]) < 0.1)
+
+	// log test results
+	if testBool {
+		t.Log("MultiVariateRandomNormal Test: Computed Mean =", testCase)
+	} else {
+		t.Error("MultiVariateRandomNormal Test: Computed Mean =", testCase)
+	}
 }
