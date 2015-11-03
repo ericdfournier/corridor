ad92ae658bb305c7354cbb43fb0fc35632e4d91c
Eric Daniel Fournier <me@ericdfournier.com>
2015-11-03 09:18:05 -0800

    Added new problem specification files for SanBernadinoValidation set

---
+++ problems/SanBernadinoValidation/sanBernadino.go
@@ -0,0 +1,89 @@
+// Copyright ©2015 The corridor Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"runtime"
+	"time"
+
+	"github.com/ericdfournier/corridor"
+)
+
+func main() {
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// set max processing units
+	cpuCount := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpuCount)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// start clock
+	start := time.Now()
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// import source subscripts
+	source := corridor.CsvToSubs("sourceSubs.csv")
+
+	// import destination subscripts
+	destination := corridor.CsvToSubs("destinationSubs.csv")
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// import domain
+	searchDomain := corridor.CsvToDomain("searchDomain.csv")
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// initialize objectives
+	searchObjectives := corridor.CsvToMultiObjective(
+		"accessibility.csv",
+		"slope.csv",
+		"disturbance.csv")
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// initialize parameters
+	populationSize := 1000
+	evolutionSize := 1000
+	randomness := 1.0
+
+	// generate parameter structure
+	searchParameters := corridor.NewParameters(
+		source,
+		destination,
+		populationSize,
+		evolutionSize,
+		randomness)
+
+	//////////////////////////////////////////////////////////////////////////////////
+
+	// evolve populations
+	searchEvolution := corridor.NewEvolution(
+		searchParameters,
+		searchDomain,
+		searchObjectives)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// initialize elite count
+	eliteCount := 100
+
+	// extract elite set
+	eliteSet := corridor.NewEliteSet(eliteCount, <-searchEvolution.Populations, searchParameters)
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// write elite set to file
+	corridor.EliteSetToCsv(eliteSet, "sanBernadino_p-100000_e-1000_eliteSet.csv")
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	// write log data to file
+	corridor.RuntimeLogToCsv(searchEvolution, time.Since(start), "log.csv")
+
+	///////////////////////////////////////////////////////////////////////////////////
+}
